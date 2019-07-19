20c17c9c092d63757ad5007b33b31d0910eb7f2e
LEE ON YOU <dhsdb1541@naver.com>
2019-07-19 15:43:40 +0900

    css 작업하기

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,1 +1,40 @@
-export default () => "Auth";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  min-height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Box = styled.div`
+  ${props => props.theme.whiteBox}
+  border-radius: 0px;
+  width: 350px;
+`;
+
+const StateChanger = styled(Box)`
+  text-align: center;
+  padding: 20px 0px;
+`;
+
+const Link = styled.span`
+  color: ${props => props.theme.blueColor};
+`;
+
+export default () => {
+  const [action, setAction] = useState("logIn");
+
+  return (
+    <Wrapper>
+      <StateChanger>
+        {action === "logIn" ? (
+          <>계정이 없으신가요? <Link onClick={() => setAction("signUp")}>가입하기</Link></>
+        ) : (
+          <>계정이 있으신가요? <Link onClick={() => setAction("logIn")}>로그인</Link></>
+        )}
+      </StateChanger>
+    </Wrapper>
+  );
+};
